8b762aff0f05d44e16105a21eda62646369b2e4a
annarm994 <155604262+annarm994@users.noreply.github.com>
2024-03-19 20:18:06 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile name="anna" email="anaa@gmail.com" />
+    </div>
+  );
+}
+
+export default App;
